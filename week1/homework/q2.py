665cc16a555849293ec6ebdd28d896f72a01232a
Taemin Lee <dlrgks88@naver.com>
2024-12-26 23:03:34 +0900

    Q. 0과 1로만 이루어진 문자열이 주어졌을 때, 이 문자열에 있는 모든 숫자를 전부 같게 만들려고 한다. 할 수 있는 행동은 문자열에서 연속된 하나 이상의 숫자를 잡고 모두 뒤집는 것이다. 뒤집는 것은 1을 0으로, 0을 1로 바꾸는 것을 의미한다.
    주어진 문자열을 모두 0 혹은 모두 1로 같게 만드는 최소 횟수를 반환하시오.

---
+++ week1/homework/q2.py
@@ -0,0 +1,29 @@
+input = "0111110"
+# 0 에서 1을 마주쳤을 때 뒤집는다. -> 전체를 0으로 만들기 위한 작업
+# 1 에서 0을 마주쳤을 때 뒤집는다. -> 전체를 1로 만들기 위한 작업
+
+
+def find_count_to_turn_out_to_all_zero_or_all_one(string):
+    count_to_all_zero = 0
+    count_to_all_one = 0
+
+    if '1' in string and '0' in string:
+        if string[0] == '0':
+            count_to_all_one += 1
+        elif string[0] == '1':
+            count_to_all_zero += 1
+
+
+    for i in range(len(string)-1):
+        if string[i] != string[i + 1]:
+            if string[i + 1] == "1":
+                count_to_all_zero += 1
+            if string[i + 1] == "0":
+                count_to_all_one += 1
+
+    print(count_to_all_one, count_to_all_zero)
+    return min(count_to_all_one, count_to_all_zero)
+
+
+result = find_count_to_turn_out_to_all_zero_or_all_one(input)
+print(result)
